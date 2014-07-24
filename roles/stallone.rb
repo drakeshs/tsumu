dd40adfaffdad2a892b88fb779af1f5a96defe9f
Jose Antonio Pio Gil <josetonyp@latizana.com>
2014-07-25 00:05:11 +0200

    Adding redid as default to stallone

--- roles/stallone.rb
+++ roles/stallone.rb
@@ -1,3 +1,10 @@
 name "stallone"
 description "Stallone role config"
-run_list "recipe[ubuntu]", "recipe[application::mysql]", "recipe[application::users]", "recipe[application::deploy]", "recipe[application::ruby-1.9]", "recipe[application::nginx]", "recipe[application::javascript]"
+run_list ["recipe[ubuntu]",
+          "recipe[application::mysql]",
+          "recipe[application::users]",
+          "recipe[application::deploy]",
+          "recipe[application::ruby-1.9]",
+          "recipe[application::nginx]",
+          "recipe[application::redis]",
+          "recipe[application::javascript]"]
