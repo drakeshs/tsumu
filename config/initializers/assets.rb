e4b7351a0d2987bc1b40044d84ddb30c08b75ccf
Jose Antonio Pio Gil <josetonyp@latizana.com>
2014-08-13 13:47:38 +0200

    Adding vendor precompilation

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,4 +5,24 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
+# Rails.application.config.assets.precompile += %w( application.scss )
+
+# This block includes all js/css files, including gems,
+# under: app/assets, vendor/assets, lib/assets
+# and excluding partial files (e.g. "_file.sass")
+config.assets.precompile << Proc.new { |path|
+  if path =~ /\.(css|js)\z/
+    full_path = Rails.application.assets.resolve(path).to_path
+    asset_paths = %w( app/assets vendor/assets/components lib/assets)
+    if ((asset_paths.any? {|ap| full_path.include? ap})
+         && !path.starts_with?('_'))
+      puts "\tIncluding: " + full_path
+      true
+    else
+      puts "\tExcluding: " + full_path
+      false
+    end
+  else
+    false
+  end
+}
