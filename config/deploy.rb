56bdd6d62108755cc0543cd3017bc8bb135b61cf
Jose Antonio Pio Gil <josetonyp@latizana.com>
2014-08-15 01:10:36 +0200

    Fix bug with sidekiq pid fle

--- config/deploy.rb
+++ config/deploy.rb
@@ -69,7 +69,7 @@ set :puma_bind, %w(tcp://0.0.0.0:9292 unix:///tmp/application.socket)
 
 
 set :sidekiq_default_hooks,  true
-set :sidekiq_pid,  File.join(shared_path, 'log', 'pids', 'sidekiq.pid')
+set :sidekiq_pid,  File.join(shared_path, 'tmp', 'pids', 'sidekiq.pid')
 set :sidekiq_env,  fetch(:rack_env, fetch(:rails_env, fetch(:stage)))
 set :sidekiq_log,  File.join(shared_path, 'log', 'sidekiq.log')
 set :sidekiq_timeout,  10
