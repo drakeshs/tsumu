6d6860fd325165c1068b0c83fa7988e7db8aa82b
Jose Antonio Pio Gil <josetonyp@latizana.com>
2014-08-15 16:26:07 +0200

    Echoing bootstraping command

--- app/models/provider/aws/box.rb
+++ app/models/provider/aws/box.rb
@@ -46,7 +46,9 @@ module Provider
         root_file = "cd #{Rails.root} &&"
         ip = Rails.env.development? ? @record.ip : @record.private_ip_address
         log = "> log/bootstrap_#{@record.name}_#{@record.application.name}_#{@record.application.eco_system.rails_environment}.log 2> log/bootstrap_#{@record.name}_#{@record.bootstrap_role}_#{@record.application.eco_system.rails_environment}_error.log"
-        system "#{root_file} knife bootstrap #{ip} -x ubuntu -i keys/#{@record.application.eco_system.key_pairs.first.name}.pem -r 'role[#{@record.bootstrap_role}]' --secret-file .chef/encrypted_data_bag_secret --sudo -E #{@record.application.eco_system.rails_environment} #{log}"
+        command = "#{root_file} knife bootstrap #{ip} -x ubuntu -i keys/#{@record.application.eco_system.key_pairs.first.name}.pem -r 'role[#{@record.bootstrap_role}]' --secret-file .chef/encrypted_data_bag_secret --sudo -E #{@record.application.eco_system.rails_environment} #{log}"
+        p command
+        system command
       end
 
 
